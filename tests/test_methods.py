fd9e2b0b7d25640735a1f0038d769c4df9bc1415
Piotr Tutak <piotr.tutak@getindata.com>
2023-06-21 16:08:14 +0200

    hello test

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,5 +1,6 @@
 from dbt_graph_builder.methods import hello_test
 
+
 def test_hello(unit_test_mocks: None):
     """
     This is a simple test, which can use a mock to override online functionality.
