d01fa82616eda58b32ea4d10dcd2b4780c099f7b
Piotr Tutak <piotr.tutak@getindata.com>
2023-06-27 15:31:57 +0200

    review fixes

--- src/dbt_graph_builder/builder.py
+++ src/dbt_graph_builder/builder.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import os
-from typing import Any, cast
+from typing import Any, cast, NamedTuple
 
 from dbt_graph_builder.gateway import GatewayConfiguration, TaskGraphConfiguration
 from dbt_graph_builder.graph import DbtManifestGraph
@@ -9,11 +9,16 @@ from dbt_graph_builder.graph import DbtManifestGraph
 LOGGER = logging.getLogger(__name__)
 
 
+class GraphConfiguration(NamedTuple):
+    """Graph configuration."""
+    enable_dags_dependencies: bool = False
+    show_ephemeral_models: bool = False
+
+
 def create_tasks_graph(
     gateway_config: GatewayConfiguration,
     manifest: dict[str, Any],
-    enable_dags_dependencies: bool,
-    show_ephemeral_models: bool,
+    graph_config: GraphConfiguration = GraphConfiguration(),
 ) -> DbtManifestGraph:
     """Create tasks graph.
 
@@ -29,11 +34,11 @@ def create_tasks_graph(
     LOGGER.info("Creating tasks graph")
     dbt_airflow_graph = DbtManifestGraph(TaskGraphConfiguration(gateway_config))
     dbt_airflow_graph.add_execution_tasks(manifest)
-    if enable_dags_dependencies:
+    if graph_config.enable_dags_dependencies:
         LOGGER.debug("Adding external dependencies")
         dbt_airflow_graph.add_external_dependencies(manifest)
-    dbt_airflow_graph.create_edges_from_dependencies(enable_dags_dependencies)
-    if not show_ephemeral_models:
+    dbt_airflow_graph.create_edges_from_dependencies(graph_config.enable_dags_dependencies)
+    if not graph_config.show_ephemeral_models:
         LOGGER.debug("Removing ephemeral nodes from graph")
         dbt_airflow_graph.remove_ephemeral_nodes_from_graph()
     LOGGER.debug("Contracting test nodes")
